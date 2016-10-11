bd7bcd0afc9490bfa8c815a614079199906a2508
Fabian Keller <hello@fabian-keller.de>
2016-10-11 14:55:14 +0200

    Feature: adding feature model for variability analysis

---
+++ src/main/java/de/fakeller/performance/variability/feature/FeatureModel.java
@@ -0,0 +1,32 @@
+package de.fakeller.performance.variability.feature;
+
+import java.util.*;
+
+/**
+ * A feature model contains all possible features of a system.
+ * <p>
+ * Constraints between features are not modeled, as they are not relevant for performance analysis. They should be
+ * modeled by the backing feature model.
+ */
+public class FeatureModel<FEATURE> {
+
+    private final Set<Feature<FEATURE>> features = new LinkedHashSet<>();
+
+    public FeatureModel(final List<Feature<FEATURE>> features) {
+        this.features.addAll(features);
+    }
+
+    /**
+     * Returns all features in this feature model.
+     */
+    public List<Feature<FEATURE>> getFeatures() {
+        return Collections.unmodifiableList(new ArrayList<Feature<FEATURE>>(this.features));
+    }
+
+    /**
+     * Determines whether the given feature is part of this feature model.
+     */
+    public boolean hasFeature(final Feature<FEATURE> feature) {
+        return this.features.contains(feature);
+    }
+}
