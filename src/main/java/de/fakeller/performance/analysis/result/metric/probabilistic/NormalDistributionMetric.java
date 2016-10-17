40d726738fe1659db9a1a43c90ca6b6ac54a23d8
Fabian Keller <hello@fabian-keller.de>
2016-10-17 23:48:17 +0200

    Test: improve metric tests

--- src/main/java/de/fakeller/performance/analysis/result/metric/probabilistic/NormalDistributionMetric.java
+++ src/main/java/de/fakeller/performance/analysis/result/metric/probabilistic/NormalDistributionMetric.java
@@ -1,13 +1,16 @@
 package de.fakeller.performance.analysis.result.metric.probabilistic;
 
+import de.fakeller.performance.analysis.result.metric.AbstractPerformanceMetric;
 import de.fakeller.performance.analysis.result.quantity.PerformanceQuantity;
 import de.fakeller.performance.analysis.result.valueobject.statistics.StandardDeviation;
 import de.fakeller.performance.analysis.result.valueobject.statistics.Variance;
 
+import java.util.Locale;
+
 /**
  * Represents a normal distribution of the metric, parametrized by the mean mu and the variance sigma.
  */
-public class NormalDistributionMetric<Q extends PerformanceQuantity> implements ProbabilisticMetric<Q> {
+public class NormalDistributionMetric<Q extends PerformanceQuantity> extends AbstractPerformanceMetric<Q> implements ProbabilisticMetric<Q> {
 
     private Q mu;
     private Variance sigma;
@@ -17,6 +20,7 @@ public class NormalDistributionMetric<Q extends PerformanceQuantity> implements 
     }
 
     public NormalDistributionMetric(Q mu, Variance sigma) {
+        super((Class<Q>) mu.getClass());
         this.mu = mu;
         this.sigma = sigma;
     }
@@ -28,7 +32,7 @@ public class NormalDistributionMetric<Q extends PerformanceQuantity> implements 
 
     @Override
     public String toHumanReadable() {
-        return String.format("N(mu=%.4f, sigma=%.4f)", mu.value(), sigma.getVariance());
+        return String.format(Locale.ROOT, "N(mu=(%s), sigma=%.4f)", mu.toHumanReadable(), sigma.getVariance());
     }
 
     public Q getMu() {
@@ -37,5 +41,24 @@ public class NormalDistributionMetric<Q extends PerformanceQuantity> implements 
 
     public Variance getSigma() {
         return sigma;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        NormalDistributionMetric<?> that = (NormalDistributionMetric<?>) o;
+
+        if (!mu.equals(that.mu)) return false;
+        return sigma.equals(that.sigma);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = mu.hashCode();
+        result = 31 * result + sigma.hashCode();
+        return result;
     }
 }
--- src/test/java/de/fakeller/performance/analysis/result/metric/aggregate/AggregateMetricTest.java
+++ src/test/java/de/fakeller/performance/analysis/result/metric/aggregate/AggregateMetricTest.java
@@ -1,10 +1,6 @@
 package de.fakeller.performance.analysis.result.metric.aggregate;
 
-import de.fakeller.performance.analysis.result.quantity.AbstractPerformanceQuantity;
-import de.fakeller.performance.analysis.result.quantity.PerformanceQuantity;
-import de.fakeller.performance.analysis.result.unit.Unitless;
-import de.fakeller.performance.analysis.result.valueobject.Percentage;
-import org.junit.Assert;
+import de.fakeller.performance.analysis.result.metric.DPQ;
 import org.junit.Test;
 
 import java.util.function.Function;
@@ -62,29 +58,4 @@ public class AggregateMetricTest {
     }
 
 
-    static class DPQ extends AbstractPerformanceQuantity<Unitless, Percentage> {
-
-        public static DPQ of(double val) {
-            return new DPQ(Percentage.of(val));
-        }
-
-        protected DPQ(Percentage value) {
-            super(value, Unitless.UNITLESS);
-        }
-
-        @Override
-        protected String getHumanReadableDescription() {
-            return "DPQ";
-        }
-
-        @Override
-        public double value() {
-            return this.value.getPercentage();
-        }
-
-        @Override
-        public PerformanceQuantity wrap(double value, Unitless unit) {
-            return new DPQ(Percentage.of(value));
-        }
-    }
 }
